abeae89a8385cce2b5d4456b1375efb543944fc7
VLDrenth <laszlodrenth@gmail.com>
2024-07-26 17:31:27 +0100

    improved saving of experiments

--- main/utils.py
+++ main/utils.py
@@ -1,18 +1,9 @@
 import pickle
+import time
+import json
+import os
 
-def save_experiment(file_name, parameters, outcome):
-    file_path = f'./experiments/{file_name}.pkl'
-    data = {
-        'parameters': parameters,
-        'outcome': outcome
-    }
-    with open(file_path, 'wb') as f:
-        pickle.dump(data, f)
+def generate_experiment_id():
+    return time.strftime("%Y%m%d-%H%M%S")
 
-def load_experiment(file_name):
-    file_path = f'./experiments/{file_name}.pkl'
-    with open(file_path, 'rb') as f:
-        data = pickle.load(f)
-    return data['parameters'], data['outcome']
-
-
+experiment_id = generate_experiment_id()
