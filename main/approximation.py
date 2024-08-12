77fc320c50b7f45e225fa5717a1cf53b417ef06e
VLDrenth <laszlodrenth@gmail.com>
2024-08-12 08:40:51 +0100

    removed unused function

--- main/approximation.py
+++ main/approximation.py
@@ -36,24 +36,4 @@ def compute_S(model, x, pseudo_label='hard_label'):
 
     hp_hat = hp[torch.arange(hp.shape[0]), label].view(hp.shape[0], -1)  # (batch, num_parameters)    precision = model.posterior_precision.to_matrix()
     S = hp_hat @ precision.inverse() @ hp_hat.T
-    return S 
-
-def compute_eig(model, x):
-    '''
-    Function to compute the expected information gain, using proposition 5.2 from Unifying_approaches_in_active_learning
-    model: laplace model
-    x: input data
-    '''
-    raise NotImplementedError  # Not sure if correct want to check first
-
-    res = torch.zeros(x.shape[0])
-    H_pp_posterior = -0.5 * model.posterior_precision.to_matrix()
-
-    pi = la(x)
-    J, _ = model.backend.last_layer_jacobians(x, enable_backprop=True)
-
-    for j in range(J.shape[0]):
-        H_pp = J[j].T @ (torch.diag(pi[j]) - pi[j] @ pi[j].T) @ J[j]  # From equation (30) in the paper
-        print(H_pp.det())
-        res[j] = 0.5 *torch.log(torch.det(H_pp @ H_pp_posterior + torch.eye(H_pp.shape[0])))  # Proposition 5.2
-    return res
+    return S  
